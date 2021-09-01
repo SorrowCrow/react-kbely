d4d54131e465e8f7f638c1c5d80017ca764bfcad
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2021-09-02 00:10:53 +0300

    react raw

--- server.js
+++ server.js
@@ -124,9 +124,9 @@ app.post("/api/stripe", async (req, res) => {
 app.use("/api/reservationItems", reservationItemRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/dist"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
     app.listen(PORT, () => console.log(`Port: ${PORT}`));
 } else {
